587daac83008507f70dea626ba0bfa0d045913f6
Souad-djerfi <bensaad.souad@gmail.com>
2023-01-20 14:14:08 +0100

    suprimer les commentaires

--- database/table.sql
+++ database/table.sql
@@ -2,7 +2,6 @@
 use mysql;
 CREATE USER app identified BY '';
 GRANT ALL privileges ON * . * TO app;
-FLUSH PRIVILEGES;
 use test;
 CREATE TABLE messages(id int PRIMARY KEY AUTO_INCREMENT, message varchar(255));
 FLUSH PRIVILEGES;
