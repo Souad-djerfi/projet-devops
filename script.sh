69d9789640a4b0904f5b2d5c02d10012e16405d5
Souad-djerfi <bensaad.souad@gmail.com>
2023-01-31 22:04:41 +0100

    ajout script.sh + Vagrantfile

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#  les adresses IP des machines virtuelles à tester
+vm_ips=("192.168.56.10" "192.168.56.11" "192.168.56.12")
+
+# Boucle sur les adresses IP des machines virtuelles
+for vm_ip in "${vm_ips[@]}"; do
+  # Utilisez la commande ping pour tester la connexion
+  ping -c 4 $vm_ip
+
+  # Vérifiez la sortie de la commande ping pour déterminer si la connexion est réussie
+  if [ $? -eq 0 ]; then
+    echo "La machine virtuelle $vm_ip est joignable"
+  else
+    echo "La machine virtuelle $vm_ip n'est pas joignable, demarrage"
+    cd vagrant
+    vagrant up
+  fi
+done
