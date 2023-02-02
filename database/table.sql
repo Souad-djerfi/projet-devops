3c153750bde742ba32b76061ee61168f15c7fd05
Afariza Halim <120117410+afarizahalim@users.noreply.github.com>
2023-02-02 09:45:30 +0100

    Delete database directory

--- database/table.sql
+++
@@ -1,7 +0,0 @@
-
-use mysql;
-CREATE USER app identified BY '';
-GRANT ALL privileges ON * . * TO app;
-use test;
-CREATE TABLE messages(id int PRIMARY KEY AUTO_INCREMENT, message varchar(255));
-FLUSH PRIVILEGES;
